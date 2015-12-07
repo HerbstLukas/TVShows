42cd4505fd51ae835b034e5c3c2e52d3f6fa9f31
Lukas Herbst <lukas.herbst@me.com>
2015-12-07 22:31:40 +0100

    Initial Commit

---
+++ TVShowsTests/TVShowsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TVShowsTests.swift
+//  TVShowsTests
+//
+//  Created by Lukas Herbst on 07.12.15.
+//  Copyright © 2015 Lukas Herbst. All rights reserved.
+//
+
+import XCTest
+@testable import TVShows
+
+class TVShowsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
